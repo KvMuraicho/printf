9e9f9b9f3931560e52be2418dda11531c9cfc4d5
KvMuraicho <muraichokelly@gmail.com>
2023-09-18 04:20:33 -0700

    fixed error

--- _printf.c
+++ _printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include"main.h"
 
 int my_printf(const char *format, ...) {
     va_list args;
+    const char *traverse;  // Declare here for C90 compatibility
+    int count = 0;  // Declare and initialize at the top of the block
+
     va_start(args, format);
     
-    int count = 0;  
-    for (const char *traverse = format; *traverse != '\0'; traverse++) {
+    for (traverse = format; *traverse != '\0'; traverse++) {
         if (*traverse != '%') {
             putchar(*traverse);
             count++;
@@ -34,6 +35,11 @@ int my_printf(const char *format, ...) {
     }
 
     va_end(args);
-    return count;  
+    return count;
+}
+
+int main() {
+    my_printf("This is a test: %d and %i\n", 1234, -5678);
+    return 0;
 }
 
