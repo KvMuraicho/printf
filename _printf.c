637276e824d43ac018c295c3c87e981947d95fda
KvMuraicho <muraichokelly@gmail.com>
2023-09-18 06:30:33 -0700

    correct _printf.c

--- _printf.c
+++ _printf.c
@@ -1,32 +1,63 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int my_printf(const char *format, ...) {
+/**
+ * _printf - custom printf function
+ * @format: the format string
+ * @...: the values to format and print
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
     va_list args;
-    const char *traverse;  
-    int count = 0;  
+    const char *traverse;
+    int count = 0;
 
     va_start(args, format);
-    
-    for (traverse = format; *traverse != '\0'; traverse++) {
-        if (*traverse != '%') {
+
+    for (traverse = format; *traverse != '\0'; traverse++)
+    {
+        
+        if (*traverse != '%')
+        {
             putchar(*traverse);
             count++;
             continue;
         }
 
+     
         traverse++;
-
-        switch (*traverse) {
-            case 'd':
-            case 'i':
+        switch (*traverse)
+        {
+            case 'c':
                 {
-                    int i = va_arg(args, int);
-                    count += printf("%d", i);
+                    char c = (char) va_arg(args, int);
+                    putchar(c);
+                    count++;
+                }
+                break;
+
+            case 's':
+                {
+                    char *s = va_arg(args, char*);
+                    while (*s)
+                    {
+                        putchar(*s);
+                        count++;
+                        s++;
+                    }
+                }
+                break;
+
+            case '%':
+                {
+                    putchar('%');
+                    count++;
                 }
                 break;
 
             default:
+          
                 putchar('%');
                 putchar(*traverse);
                 count += 2;
@@ -35,11 +66,13 @@ int my_printf(const char *format, ...) {
     }
 
     va_end(args);
+
     return count;
 }
 
-int main() {
-    my_printf("This is a test: %d and %i\n", 1234, -5678);
+int main(void)
+{
+    _printf("Character: %c, String: %s, Percent sign: %%\n", 'A', "Hello");
     return 0;
 }
 
