be46ee13bd8f2c9dbea944d7524ac57985b075cd
KvMuraicho <muraichokelly@gmail.com>
2023-09-19 04:29:04 -0700

    betty fix

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 #define F_MINUS 1
@@ -12,41 +13,25 @@
 #define F_SPACE 16
 #define S_LONG 2
 #define S_SHORT 1
+
 /**
-
- * struct fmt - Struct op
-
- *
-
- * @fmt: The format.
-
- * @fn: The function associated.
-
- */
-
+* struct fmt - Struct op
+*
+* @fmt: The format.
+* @fn: The function associated.
+*/
 struct fmt
-
 {
-
-        char fmt;
-
-        int (*fn)(va_list, char[], int, int, int, int);
-
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
 };
 
-
-
 /**
-
- * typedef struct fmt fmt_t - Struct op
-
- *
-
- * @fmt: The format.
-
- * @fm_t: The function associated.
-
- */
+* typedef struct fmt fmt_t - Struct op
+*
+* @fmt: The format.
+* @fm_t: The function associated.
+*/
 
 typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
